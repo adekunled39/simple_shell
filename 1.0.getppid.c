2d06cc90084d7bbde851877b4b3748114ff332c6
adekunled39 <adekunled39@gmail.com>
2022-06-15 13:39:08 -0700

    update file

--- 1.0.getppid.c
+++ 1.0.getppid.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	pid_t my_pid;
+pid_t my_pid;
 
-	my_pid = getpid();
-	printf("hijo %u\n", my_pid);
-	my_pid = getppid();
-	printf("padre %u\n", my_pid);
-	return (0);
+my_pid = getpid();
+printf("hijo %u\n", my_pid);
+my_pid = getppid();
+printf("padre %u\n", my_pid);
+return (0);
 }
